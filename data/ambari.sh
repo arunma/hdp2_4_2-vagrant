d8c022f83c693f88db80503b9ecf075482eba370
Arun Manivannan <arun@arunma.com>
2017-06-28 23:10:05 +0800

    Fixing carriage return issues with windows.

--- data/ambari.sh
+++ data/ambari.sh
@@ -1,10 +1,11 @@
 echo "EXECUTING AMBARI SCRIPT !!!"
 sudo ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
 sudo cat /root/.ssh/id_rsa.pub
-sudo cp /root/.ssh/* /vagrant/
+sudo cp /root/.ssh/* /vagrant
 sudo cat /vagrant/id_rsa.pub >> /root/.ssh/authorized_keys
-sudo cd /etc/yum.repos.d
-sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.4.0.1/ambari.repo -O /etc/yum.repos.d/ambari.repo
+sudo cd /vagrant
+sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.4.0.1/ambari.repo
+cp -f ambari.repo /etc/yum.repos.d/ambari.repo
 sudo yum -y install ambari-server
 sudo ambari-server setup -s
 sudo ambari-server start
