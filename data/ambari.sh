ba4dd11062a790f57b3ad777648614f190da8919
Arun Manivannan <arun@arunma.com>
2018-05-29 23:11:33 +0800

    Updated vagrant files

--- data/ambari.sh
+++ data/ambari.sh
@@ -4,10 +4,16 @@ sudo cat /root/.ssh/id_rsa.pub
 sudo cp /root/.ssh/* /vagrant_data
 sudo cat /vagrant_data/id_rsa.pub >> /root/.ssh/authorized_keys
 sudo cd /vagrant_data
-#sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.4.0.1/ambari.repo
-sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.6.0.0/ambari.repo
+#sudo cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.6.1.5/ambari.repo
 cp -f ambari.repo /etc/yum.repos.d/ambari.repo
 sudo yum -y install ambari-server
 sudo ambari-server setup -s
 sudo ambari-server start
+sudo wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+sudo yum update
+sudo yum install -y mysql-connector-java
+sudo ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar 
+sudo systemctl start mysqld.service
 echo "DONE. PLEASE BRING UP THE OTHER VMs NOW."
